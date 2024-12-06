a77e58081637063bbdce0936f67bf116282a0b3e
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-06 12:05:27 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class set{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+      int c=0;
+      while(n>0){
+        c+=n&1;
+        n>>=1;
+      }
+      System.out.println(c);
+    }
+}
